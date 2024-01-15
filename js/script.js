897089cadac1dccaa0cc8d819254d6733412678a
Polvere243 <danielercoli@gmail.com>
2024-01-15 01:59:59 +0100

    got the elements from the DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-console.log("JS OK");
+// recupero gli elementi dalla pagina
+const listOfNumbers = document.getElementById("numbers");
+const answersField = document.getElementById("answers");
+console.log(listOfNumbers, answersField);
