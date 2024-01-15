7a156bfa827669a8e11b9511457d68b81a364464
Polvere243 <danielercoli@gmail.com>
2024-01-15 02:12:56 +0100

    showed 5 random numbers in page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 // recupero gli elementi dalla pagina
 const listOfNumbers = document.getElementById("numbers");
 const answersField = document.getElementById("answers");
-console.log(listOfNumbers, answersField);
+const numberOfNumbers = 5;
+// funzione per generare un numero casuale
+const getRandomNumber = (max) => {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+for (let i = 0; i < numberOfNumbers; i++) {
+    const randomNumber = getRandomNumber(100);
+    const listItem = `<li>${randomNumber}</li>`
+    listOfNumbers.innerHTML += listItem;
+}
