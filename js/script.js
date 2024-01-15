cfa1192358e240b00d1eb0b8ebc87ef32f3457b6
Polvere243 <danielercoli@gmail.com>
2024-01-15 18:24:20 +0100

    injected a dinamic text into the h1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
 // recupero gli elementi dalla pagina
+const titleElement = document.querySelector("h1");
 const listOfNumbers = document.getElementById("numbers");
 const answersField = document.getElementById("answers");
+const scoreElement = document.getElementById("score-message") 
 const numberOfNumbers = 5;
+const seconds = 30;
+// genero un titolo dinamico 
+titleElement.innerText = `Memorizzali! Hai solo ${seconds} secondi.`;
 // funzione per generare un numero casuale
 const getRandomNumber = (max) => {
     return Math.floor(Math.random() * max) + 1;
 }
-
+const randomNumbers =[];
 for (let i = 0; i < numberOfNumbers; i++) {
     const randomNumber = getRandomNumber(100);
     const listItem = `<li>${randomNumber}</li>`
+    randomNumbers.push.apply(randomNumber);
     listOfNumbers.innerHTML += listItem;
 }
+
+setTimeout (function(){
+    listOfNumbers.classList.add("d-none");
+    answersField.classList.remove("d-none")
+});
+
+answersField.addEventListener ("submit", function(e){
+    e.preventDefault;
+    
+})
+
+
+
